12f5f6d469bcb32646b11ab6aaaa4e4da4e7bd59
CreativeCoder-90 <76105912+CreativeCoder-90@users.noreply.github.com>
2021-01-20 15:03:16 +0530

    Update AssignmentFirstQ1.java
    //program to find the second smallest element in an array.
    
    //Soultion Approch :
    /*
         input : []
         //First make sure array has two items.
         // [2,5,1]
         // Sort ascending / descending
         [1,2 ,5] arr[1]
         [5 ,2 ,1]
     */

--- src/day1/AssignmentFirstQ1.java
+++ src/day1/AssignmentFirstQ1.java
@@ -1,25 +1,28 @@
-
+package day1;
 //program to find the second smallest element in an array.
 
 class AssignmentFirst {
 
     public static void main(String[] args) {
 
-        int[] arr = {12,5,8,3,11,36};
-        int smallest = Integer.MAX_VALUE;
-        int s_smallest = Integer.MAX_VALUE;  // second smallest
+        int[] arr = {0, -1, 8, 3, 11, 8, 5};
+        if (arr.length >= 2) {
+            int smallest = Integer.MAX_VALUE;
+            int s_smallest = Integer.MAX_VALUE;  // second smallest
 
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<smallest){
-                smallest=arr[i];
-            }
-        }
 
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<s_smallest && arr[i]>smallest){
-                s_smallest = arr[i];
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < smallest) {
+                    smallest = arr[i];
+                }
             }
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < s_smallest && arr[i] > smallest) {
+                    s_smallest = arr[i];
+                }
+            }
+            System.out.println(s_smallest);
         }
-        System.out.println(s_smallest);
     }
 }
